faaf50e0656beee2eace5fc444a089113bc5ee6c
DevShashi1993 <shashikantsharma1993@gmail.com>
2020-09-25 02:21:50 +0530

    Layout Changes

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React, {useEffect, useLayoutEffect, useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { WEATHER_API_KEY } from 'react-native-dotenv'
+import { Button, StatusBar, StyleSheet, Text, View, Switch , Platform } from 'react-native';
+import { Card, Title, Paragraph } from 'react-native-paper';
+import Constants from "expo-constants";
 import * as Location from 'expo-location';
 
-// const WEATHER_API_KEY = '6ba2d126dc7cc5c8553e08078ef62590';
-const apiKey = process.env['WEATHER_API_KEY'];
-console.log('WEATHER_API_KEY = ', WEATHER_API_KEY);
-const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?';
-
+const WEATHER_API_KEY = '6ba2d126dc7cc5c8553e08078ef62590';
+const BASE_WEATHER_URL='https://api.openweathermap.org/data/2.5/weather?';
 const App = () => {
-
   const [errorMessage, setErrorMessage] = useState(null);
   const [currentWeather, setCurrentWeather] = useState(null);
   const [unitSystem, setUnitSystem] = useState('metric');
 
-   const load = async () => {
+  const load = async () => {
     try {
       let { status } = await Location.requestPermissionsAsync();
       if(status !== 'granted'){
@@ -24,7 +20,6 @@ const App = () => {
       }
       const location = await Location.getCurrentPositionAsync(); 
       const {latitude, longitude} = location.coords;
-      const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?';
       const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitSystem}&appid=${WEATHER_API_KEY}`;
       
       const response = await fetch(weatherUrl);
@@ -44,31 +39,51 @@ const App = () => {
     load();
   }, []);
 
-  if(currentWeather) {
-    const { main: {temp} } = currentWeather;
-    return (
+  return (
+    <>
+      <StatusBar backgroundColor="grey" barStyle="default" />
+        <Card>
+          <Card.Content>
+            <Title>Climate App</Title>
+            <Paragraph>Displays temperature based on the device current Location</Paragraph>
+          </Card.Content>
+        </Card>
+
       <View style={styles.container}>
-        <Text>Weather of your current location is</Text>
-        <Text>{temp} 'Celsius</Text>
-        <StatusBar style="auto" />
+        {currentWeather ? (
+          <>
+            <Text style={styles.textStyle}>
+              Weather of your current location is
+            </Text>
+            <Text style={styles.biggerText}>
+              {currentWeather.main.temp} &deg;C
+            </Text>
+          </>
+        ) : (
+          <>
+            <Text>{errorMessage}</Text>
+          </>
+        )}
       </View>
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        <Text>{errorMessage}</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
-}
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ECF0F1',
+  },
+  textStyle:{
+    fontSize: 15,
+    textAlign: 'center'
+  },
+  biggerText:{
+    fontSize: 35,
+    textAlign: 'center'
   },
 });
 
